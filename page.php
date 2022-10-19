9acde034448e1ef0e9d3c36888bbeb397d4e58da
CooperChristenson <112706771+CooperChristenson@users.noreply.github.com>
2022-10-19 13:47:24 -0500

    Add files via upload

--- page.php
+++ page.php
@@ -13,18 +13,7 @@
 								<?php the_content(); ?>
 							</div>
 						</div>
-						<div class="card-footer">
-							<div class="post-date date">
-								<time datetime="<?php the_time('Y-m-d\TH:i:sP') ?>" itemprop="datePublished">
-									Published <?php the_time('F jS, Y') ?>
-								</time>
-								<?php
-									if(is_user_logged_in()){
-										echo edit_post_link('edit').'<br> by '.get_the_author();
-									}
-								?>
-							</div>
-						</div>
+						
 					</div>
 				</div>
 			<?php endwhile; else: ?>
